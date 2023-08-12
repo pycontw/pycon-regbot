ef9f238771a43ddbb7d2924655152e354d430215
davidtnfsh <davidtnfsh@gmail.com>
2023-08-12 15:25:36 +0800

    [feat](token_import): read qrcode from bigquery

--- token_import.py
+++ token_import.py
@@ -1,40 +1,19 @@
 import os
-import csv
+from datetime import datetime
 from dotenv import load_dotenv
+from google.cloud import bigquery
+import requests
 load_dotenv()
 
 # Defined in .env file
-CORPORATE_FILE = os.getenv("CORPORATE_FILE_PATH")
-INDIVIDUAL_FILE = os.getenv("INDIVIDUAL_FILE_PATH")
-RESERVED_FILE = os.getenv("RESERVED_FILE_PATH")
 USED_FILE = os.getenv("USED_FILE_PATH")
+BIQUERY_CLIENT = bigquery.Client(project=os.getenv("BIGQUERY_PROJECT"))
 
 def import_all_token():
     token_dict = dict()
-    
-    # Corporate Ticket
-    with open(CORPORATE_FILE, newline='') as f:
-        rows = csv.reader(f)
-        for row in rows:
-            token = row[0]
-            if len(token) == 32:
-                token_dict[token] = "corporate"
-
-    # Individual Ticket
-    with open(INDIVIDUAL_FILE, newline='') as f:
-        rows = csv.reader(f)
-        for row in rows:
-            token = row[0]
-            if len(token) == 32:
-                token_dict[token] = "individual"
-
-    # Reserved Ticket
-    with open(RESERVED_FILE, newline='') as f:
-        rows = csv.reader(f)
-        for token, ticket_type in rows:
-            if len(token) == 32:
-                token_dict[token] = ticket_type
-
+    token_dict = _import_qrcode_from_bigquery(token_dict, table_name='dwd.kktix_ticket_corporate_attendees')
+    token_dict = _import_qrcode_from_bigquery(token_dict, table_name='dwd.kktix_ticket_individual_attendees')
+    token_dict = _import_qrcode_from_bigquery(token_dict, table_name='dwd.kktix_ticket_reserved_attendees')
     return token_dict
 
 def read_used_list():
@@ -47,3 +26,34 @@ def read_used_list():
         f = open(USED_FILE, 'w+')
         f.close()
         return list()
+
+def _import_qrcode_from_bigquery(token_dict: dict, table_name: str) -> dict:
+    current_year = datetime.now().year
+    query = f"""
+        SELECT
+          ticket_type,
+          qrcode
+        FROM
+          `pycontw-225217.{table_name}`
+        WHERE
+          qrcode IS NOT NULL
+          AND payment_status = 'paid'
+          AND EXTRACT(YEAR
+          FROM
+            CAST(paid_date AS DATE)) = {current_year};
+    """
+    query_job = BIQUERY_CLIENT.query(query)
+    for row in query_job:
+        ticket_name = row["ticket_type"].lower().replace("\"", "")
+        qrcode = row["qrcode"].replace("\"", "")
+        if len(qrcode) != 32:
+            requests.post(
+                os.getenv("DISCORD_WEBHOOK"),
+                json={
+                    "username": "大會當天的 kktix 註冊機器人",
+                    "content": f"Invalid qrcode: {qrcode}! It's length should be 32",
+                },
+            )
+            continue
+        token_dict[qrcode] = ticket_name
+    return token_dict
